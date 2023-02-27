cd31ae63f0570175947e45ff9c8c04874b10b003
Cong-Thuan DO <thuan@GMMBP11.local>
2023-02-27 22:34:41 +0100

    feat(navbar): create mobile menu and add js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            screens: {
+              sm: "375px",
+            },
+            fontFamily: {
+                inter: ["Inter", "sans-serif"],
+            },
+            colors: {
+                orange: "hsl(31, 77%, 52%)",
+                red: "hsl(5, 85%, 63%)",
+                offWhite: "hsl(36, 100%, 99%)",
+                grayishBlue: "hsl(233, 8%, 79%)",
+                darkGrayishBlue: "hsl(236, 13%, 42%)",
+                veryDarkBlue: "hsl(240, 100%, 5%)",
+            },
+        },
+    },
+    plugins: [],
+};
